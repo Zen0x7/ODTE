453121112c148c4b6950b66ce41467cc5ead7225
Ian Torres <iantorres@outlook.com>
2026-09-11 10:20:01 -0300

    feat: add odte::get_version() + codecov.yml exclusions
    - version.hpp: constexpr get_version() retorna string_view
    - codecov.yml: excluye tests/, third_party/, build/

--- include/odte/version.hpp
+++ include/odte/version.hpp
@@ -1,5 +1,15 @@
 #pragma once
 
-constexpr int ODTE_VERSION_MAJOR = 0;
-constexpr int ODTE_VERSION_MINOR = 0;
-constexpr int ODTE_VERSION_PATCH = 0;
+#include <string_view>
+
+namespace odte {
+
+constexpr int VERSION_MAJOR = 0;
+constexpr int VERSION_MINOR = 0;
+constexpr int VERSION_PATCH = 0;
+
+constexpr std::string_view get_version() {
+    return "0.0.0";
+}
+
+} // namespace odte
